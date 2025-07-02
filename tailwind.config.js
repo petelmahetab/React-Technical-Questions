b6c6e566a9dbdfc1c96e1b516106d871a65f86b7
Mahetab Patel <patelmahetab9020@gmail.com>
2025-07-02 23:23:48 +0530

    Changes'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
+
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-=======
   content: [],
->>>>>>> caa7e10389401d89f40c5db375e6012878898292
   theme: {
     extend: {},
   },
   plugins: [],
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> caa7e10389401d89f40c5db375e6012878898292
